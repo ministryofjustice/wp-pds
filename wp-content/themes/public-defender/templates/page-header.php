0a4ac4d7a9a522047abfb7c911343d5fe32008f3
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2016-06-21 22:08:56 +0100

    Fix bug with broken page headers.

--- wp-content/themes/public-defender/templates/page-header.php
+++ wp-content/themes/public-defender/templates/page-header.php
@@ -1,4 +1,4 @@
-<?php if(!is_home): ?><div id="page-header"></div><?php endif; ?>
+<?php if(!is_home()): ?><div id="page-header"></div><?php endif; ?>
 <div class="page-header">
     <h1>
         <?php echo roots_title(); ?>

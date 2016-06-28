99614351aaa8864eb76a3c70a6e3508d9feb35dd
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2016-06-28 14:47:27 +0100

    Create shortcode [solicitors_offices] for displaying solicitor office locations. Replace custom query in 'Our Offices' page template with use of shortcode.

--- wp-content/themes/public-defender/lib/shortcodes.php
+++ wp-content/themes/public-defender/lib/shortcodes.php
@@ -6,7 +6,7 @@
 
 function sc_solicitors_offices($atts) {
   ob_start();
-  template_part('templates/shortcodes/solicitors_offices');
+  template_part('templates/shortcodes/solicitors_offices', compact('atts'));
   return ob_get_clean();
 }
 add_shortcode('solicitors_offices', 'sc_solicitors_offices');
--- wp-content/themes/public-defender/templates/shortcodes/solicitors_offices.php
+++ wp-content/themes/public-defender/templates/shortcodes/solicitors_offices.php
@@ -2,15 +2,59 @@
 /**
  * Display office locations in a grid.
  * Snippet tag: [solicitors_offices]
+ * Attributes:
+ *  - col-md (optional) Number of columns wide each office should be.
  */
+?>
+
+<?php
+
+$offices_page = new WP_Query(array(
+  'post_type' => 'page',
+  'post_status' => 'publish',
+  'meta_key' => '_wp_page_template',
+  'meta_value' => 'template-our-offices-page.php',
+));
+$offices_page = $offices_page->post;
 
 $args = array(
   'post_type' => 'page',
   'post_status' => 'publish',
+  'post_parent' => $offices_page->ID,
+  'posts_per_page' => -1,
+  'orderby' => 'title',
+  'order' => 'ASC',
 );
 
+$offices = new WP_Query($args);
+
 ?>
+<?php if ($offices->have_posts()): ?>
+  <div class="row">
+    <?php while ($offices->have_posts()): $offices->the_post(); ?>
+      <?php
 
-<div class="row">
+      if (has_post_thumbnail()) {
+        $image_id = get_post_thumbnail_id();
+        $image = acf_get_attachment($image_id);
+        $image_url = esc_url($image['sizes']['deep_link']);
+        $style = "background-image: url('{$image_url}');";
+      }
+      else {
+        $style = false;
+      }
 
-</div>
+      $col_md = isset($atts['col-md']) ? $atts['col-md'] : 6;
+
+      ?>
+      <div class="col-md-<?php echo $col_md; ?> col-sm-6">
+        <a href="<?php the_permalink(); ?>" class="deep-link" <?php if ($style) echo 'style="' . $style . '"'; ?>>
+          <div class="deep-link__title">
+            <?php the_title(); ?>
+          </div>
+        </a>
+      </div>
+    <?php endwhile; ?>
+  </div>
+<?php endif; ?>
+<?php wp_reset_postdata(); ?>

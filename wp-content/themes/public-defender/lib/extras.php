570f752b38871531b6e12a928b5e42b367172c5b
Ollie Treend <ollie@treend.com>
2016-07-04 22:11:17 +0100

    Remove OptionTree from theme.

--- wp-content/themes/public-defender/lib/extras.php
+++ wp-content/themes/public-defender/lib/extras.php
@@ -64,37 +64,6 @@ function is_tree($pid) {
     }
 }
 
-/* Setup option tree */
-add_filter('ot_theme_mode', '__return_true');
-add_filter('ot_show_pages', '__return_false');
-add_filter('ot_show_new_layout', '__return_false');
-add_filter('ot_use_theme_options', '__return_false');
-
-add_filter('ot_header_version_text', '__return_null');
-
-//load_template( trailingslashit( get_template_directory() ) . 'inc/theme-options.php' );
-require_once (trailingslashit(get_template_directory()) . 'option-tree/ot-loader.php');
-
-/**
- * Meta Boxes
- */
-load_template(trailingslashit(get_template_directory()) . 'lib/meta-boxes.php');
-
-// Prevent default fields in optiontree listitem
-add_filter('ot_list_item_settings', 'filter_ot_list_item_settings', 10, 2);
-
-function filter_ot_list_item_settings($settings, $id) {
-
-    // Only remove settings from a specific option
-    if (in_array($id, array('solicitor-advocates', 'duty-solicitors', 'police-reps')))
-        return array();
-
-    return $settings;
-}
-
-// Overwrites WPEngine's option to turn RAND ordering off
-update_option('wpe-rand-enabled', 1);
-
 // Replaces menu_order with two meta_value for sorting by multiple custom fields
 function orderbyreplace($orderby) {
     return str_replace('wp_posts.menu_order', 'wp_postmeta.meta_value, mt1.meta_value', $orderby);

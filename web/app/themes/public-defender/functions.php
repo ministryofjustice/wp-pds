c033da6397429b1ab5e867b8d022a30ff3700e7d
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2019-09-03 13:28:25 +0100

    Fix multiple function require

--- web/app/themes/public-defender/functions.php
+++ web/app/themes/public-defender/functions.php
@@ -36,26 +36,7 @@ foreach ($roots_includes as $file) {
         trigger_error(sprintf(__('Error locating %s for inclusion', 'roots'), $file), E_USER_ERROR);
     }
 
-    require_once $filepath;
+    require_once($filepath);
 }
 unset($file, $filepath);
 
-/**
- * Get the current version of WP
- *
- * This is provided for external resources to resolve the current wp_version
- *
- * @return string
- */
-function moj_wp_version()
-{
-    global $wp_version;
-    return $wp_version;
-}
-
-add_action('rest_api_init', function () {
-    register_rest_route('moj', '/version', array(
-        'methods' => 'GET',
-        'callback' => 'moj_wp_version'
-    ));
-});

4eb2a6d32c80bc2ed1d578994eaa0bd07f15bb83
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2019-12-11 14:26:35 +0000

    Header Fix

--- web/app/themes/public-defender/base.php
+++ web/app/themes/public-defender/base.php
@@ -1,34 +1,8 @@
+
 <?php
-
-$class = array();
-
-$section_class_map = array(
-  'advocates' => 'advocate-pages',
-  'solicitors' => 'solicitors-pages',
-);
-
-$section = get_site_section();
-if ($section) {
-  $class[] = $section_class_map[$section];
-}
-
-if ($post && $post->post_type == 'page') {
-  $class[] = $post->post_name;
-}
-
+do_action('get_header');
+get_header();
 ?>
-<body <?php body_class(implode(' ', $class)); ?>>
-
-    <!--[if lt IE 9]>
-      <div class="alert alert-warning">
-        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
-      </div>
-    <![endif]-->
-
-    <?php
-    do_action('get_header');
-    get_header();
-    ?>
 
     <article class="wrap container" role="document">
       <div class="row">
@@ -38,7 +12,4 @@ if ($post && $post->post_type == 'page') {
       </div>
     </article><!-- /.wrap -->
 
-    <?php get_footer(); ?>
-
-</body>
-</html>
+<?php get_footer(); ?>
--- web/app/themes/public-defender/header.php
+++ web/app/themes/public-defender/header.php
@@ -1,4 +1,53 @@
-<?php get_template_part('head'); ?>
+<!doctype html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <?php wp_head(); ?>
+
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
+
+    <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
+</head>
+
+<?php
+
+$class = array();
+
+$section_class_map = array(
+    'advocates' => 'advocate-pages',
+    'solicitors' => 'solicitors-pages',
+);
+
+$section = get_site_section();
+if ($section) {
+    $class[] = $section_class_map[$section];
+}
+
+if ($post && $post->post_type == 'page') {
+    $class[] = $post->post_name;
+}
+
+?>
+<body <?php body_class(implode(' ', $class)); ?>>
+
+<!--[if lt IE 9]>
+<div class="alert alert-warning">
+    <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
+</div>
+<![endif]-->
+
 <header class="site-header navbar navbar-default navbar-static-top" role="banner">
     <div class="container">
         <div class="navbar-header">
